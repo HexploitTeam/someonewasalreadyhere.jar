36d4fdff5321037eb663ac52508d81c4aee40c25
Hexploit <hexploitteam@gmail.com>
2026-06-27 20:33:35 +0330

    Add main mod class SomeoneWasAlreadyHere
    Introduce the main Forge mod entrypoint SomeoneWasAlreadyHere. The constructor registers item, entity and sound DeferredRegisters to the mod event bus, initializes the mod network and an Echo capability, and registers event handlers on the MinecraftForge event bus.
    
    Note: the constructor currently calls MinecraftForge.EVENT_BUS.register(new SomeoneWasAlreadyHere()), which will recursively construct the class and lead to a stack overflow; this should be replaced with registering 'this' or removed.

---
+++ src/main/java/com/someonewasalreadyhere/SomeoneWasAlreadyHere.java
@@ -0,0 +1,34 @@
+package com.someonewasalreadyhere;
+
+import com.someonewasalreadyhere.core.network.ModNetwork;
+import com.someonewasalreadyhere.core.registry.ModEntities;
+import com.someonewasalreadyhere.core.registry.ModItems;
+import com.someonewasalreadyhere.core.registry.ModSounds;
+import com.someonewasalreadyhere.system.awareness.AwarenessEventHandler;
+import com.someonewasalreadyhere.system.echo.EchoCapability;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(SomeoneWasAlreadyHere.MODID)
+public class SomeoneWasAlreadyHere {
+
+    public static final String MODID = "someonewasalreadyhere";
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
+
+    public SomeoneWasAlreadyHere() {
+        var modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.ITEMS.register(modEventBus);
+        ModEntities.ENTITIES.register(modEventBus);
+        ModSounds.SOUNDS.register(modEventBus);
+
+        ModNetwork.register();
+        EchoCapability.register(modEventBus);
+
+        MinecraftForge.EVENT_BUS.register(AwarenessEventHandler.class);
+        MinecraftForge.EVENT_BUS.register(new SomeoneWasAlreadyHere());
+    }
+}
