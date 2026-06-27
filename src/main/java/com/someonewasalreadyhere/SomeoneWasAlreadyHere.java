2e9d2e0d48054ca4ec1b002ebe7863aad2df542a
Hexploit <hexploitteam@gmail.com>
2026-06-27 21:13:22 +0330

    Register creative tab and custom loot conditions
    Add registration of ModCreativeTab and ModLootConditions on the mod event bus and include their imports. Organize startup code with brief comments, ensure ModNetwork and EchoCapability are registered, and register AwarenessEventHandler and the mod instance on the Forge event bus.

--- src/main/java/com/someonewasalreadyhere/SomeoneWasAlreadyHere.java
+++ src/main/java/com/someonewasalreadyhere/SomeoneWasAlreadyHere.java
@@ -1,11 +1,13 @@
 package com.someonewasalreadyhere;
 
 import com.someonewasalreadyhere.core.network.ModNetwork;
+import com.someonewasalreadyhere.core.registry.ModCreativeTab;
 import com.someonewasalreadyhere.core.registry.ModEntities;
 import com.someonewasalreadyhere.core.registry.ModItems;
 import com.someonewasalreadyhere.core.registry.ModSounds;
 import com.someonewasalreadyhere.system.awareness.AwarenessEventHandler;
 import com.someonewasalreadyhere.system.echo.EchoCapability;
+import com.someonewasalreadyhere.system.loot.ModLootConditions;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -21,13 +23,22 @@ public class SomeoneWasAlreadyHere {
     public SomeoneWasAlreadyHere() {
         var modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        // Core registries
         ModItems.ITEMS.register(modEventBus);
         ModEntities.ENTITIES.register(modEventBus);
         ModSounds.SOUNDS.register(modEventBus);
 
+        // Creative tab
+        ModCreativeTab.CREATIVE_TABS.register(modEventBus);
+
+        // Custom loot conditions
+        ModLootConditions.LOOT_CONDITIONS.register(modEventBus);
+
+        // Network + capabilities
         ModNetwork.register();
         EchoCapability.register(modEventBus);
 
+        // Forge event bus listeners
         MinecraftForge.EVENT_BUS.register(AwarenessEventHandler.class);
         MinecraftForge.EVENT_BUS.register(new SomeoneWasAlreadyHere());
     }
